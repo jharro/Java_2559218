40a2a6d3a349eb0b437315b7778565e27486f8e0
jharro <jharro@gmail.com>
2022-10-13 09:01:07 -0500

    Se crean las clases Math y String

---
+++ src/fundamentos/Clase_Math.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Clase_Math {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2,5));
+        System.out.println(Math.ceil(456.98));
+        System.out.println(Math.floor(456.98));
+        System.out.println(Math.pow(8, (double)1/3));
+        System.out.println(Math.abs(-5));
+        System.out.println(Math.random()*(200-180+1)+180);
+    }
+}
---
+++ src/fundamentos/Clase_string.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Clase_string {
+    public static void main(String[] args) {
+        String mensaje1="   Hoy es...   ";
+        String mensaje2="  jueves   ";
+        System.out.println(mensaje1.concat(mensaje2));
+        System.out.println(mensaje2.endsWith("f"));
+        String sinEspacios=mensaje1.trim();
+        System.out.println(sinEspacios);
+    }
+}
